daa83f4ab9206cbf748b8823e464b82ccfc444f9
shirleys <shirleyshaw.is@gmail.com>
2019-12-30 00:20:53 -0800

    extract redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,8 +24,8 @@ class ArticlesController < ApplicationController
   # POST /articles
   # POST /articles.json
   def create
+    # return render plain: params[:article].inspect
     @article = Article.new(article_params)
-
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
