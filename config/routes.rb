760f62955f794216218069ebb484a7ed1966c29a
shirleys <shirleyshaw.is@gmail.com>
2020-01-01 15:55:33 -0800

    signup users from ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :users
-  resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :comments
+  # resources :users
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'user#create'
+  resources :users, except: [:new]
 end
