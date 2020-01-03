af28c46e94d3b98e8b9e2855fafdcd42e294565f
shirleys <shirleyshaw.is@gmail.com>
2020-01-02 17:04:46 -0800

    complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_same_user, only:[:edit, :update]
   # GET /users
   # GET /users.json
   def index
@@ -81,5 +81,13 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "you can only edit your own account"
+        redirect_to root_path
+      end
+
     end
 end
